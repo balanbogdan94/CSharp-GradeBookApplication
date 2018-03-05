5892c12032f409d5f9f52c001412725573d51a7e
Bogdan Balan <bogdan.balan@cerner.com>
2018-03-05 17:02:41 +0200

    OverrideGetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -10,5 +11,24 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException();
+            int trashold = (int)Math.Round(Students.Count * (0.2));
+            var sortedStudentList = Students.OrderByDescending(x => x.AverageGrade).Select(x => x.AverageGrade).ToList();
+            if ((sortedStudentList[trashold - 1]) <= averageGrade)
+                return 'A';
+            else if ((sortedStudentList[(trashold * 2) - 1]) <= averageGrade)
+                return 'B';
+            else if ((sortedStudentList[(trashold * 3) - 1]) <= averageGrade)
+                return 'C';
+            else if ((sortedStudentList[(trashold * 4) - 1]) <= averageGrade)
+                return 'D';
+            return 'F';
+        }
+
     }
 }
